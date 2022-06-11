8392831703174af6d71b6bbbf185107f059e7540
Ishita Jalla <ishitajalla@gmail.com>
2022-06-11 17:08:29 +0530

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learningdart/firebase_options.dart';
+import 'package:learningdart/views/login_view.dart';
+import 'package:learningdart/views/register_view.dart';
+import 'package:learningdart/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +15,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -21,32 +28,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              //the user may or may not be there or anonymous users are disabled then there might be no user or null
-              //so hence we need to do the null check
-              //so if cond means that if the user exists take that value else take a value false
-              if (user?.emailVerified ?? false) {
-                print('you are a verified user');
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            //the user may or may not be there or anonymous users are disabled then there might be no user or null
+            //so hence we need to do the null check
+            //so if cond means that if the user exists take that value else take a value false
+            //print(user);
+            if (user != null) {
+              if (user.emailVerified) {
+                print('email is verified');
               } else {
-                print('you need to verify your email first');
+                return const VerifyEmailView();
               }
-              return const Text('Done');
-            default:
-              return const Text('Loading.....');
-          }
-        },
-      ),
+            } else {
+              return const LoginView();
+            }
+
+            return const Text('Done');
+
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
